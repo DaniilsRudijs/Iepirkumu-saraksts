97b07f3b0e0a8eac7e91bad8e630b865ebc078b7
Daniils Rudijs <dandaniilin@gmail.com>
2021-04-20 17:00:14 +0300

    Pop-Up pievienosana

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('jaunsPirkums').addEventListener('click', () => {
+    console.log("Poga nospiesta!");
+    
+    let popUp = document.getElementById('popUp');
+    
+    popUp.style.display = 'block'
+});
