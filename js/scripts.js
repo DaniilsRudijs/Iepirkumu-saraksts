8d8c69038c72d68fb4802442aa0382b258f0fed6
Daniils Rudijs <daniils.rudijsdp11@gmail.com>
2021-04-27 12:53:47 +0300

    Speja pievienot pirkumus

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
+const popUp = document.getElementById('popUp');
+let ieraksti = [];
+
 document.getElementById('jaunsPirkums').addEventListener('click', () => {
-    console.log("Poga nospiesta!");
-    
-    let popUp = document.getElementById('popUp');
-    
-    popUp.style.display = 'block'
+    popUp.style.display = 'block';
 });
+
+document.getElementById('pievienotPirkumu').addEventListener('click', () => {
+    popUp.style.display = 'none';
+
+    let pirkums = {Nosaukums: precesNosaukums.value, Daudzums: daudzums.value};
+
+    precesNosaukums.value = "";
+    daudzums.value = "";
+
+    ieraksti.push(pirkums);
+
+    render();
+ });
+
+ function render() {
+    let saraksts = document.getElementById('saraksts');
+    saraksts.innerHTML = "";
+
+    for(let i = 0; i < ieraksti.length; i++) {
+        let pirkums = `
+        <div class="pirkums">
+            <h3>Prece: ${ieraksti[i].precesNosaukums}</h3>
+            <h4>Daudzums: ${ieraksti[i].daudzums}</h4>
+        </div>`;
+
+        saraksts.innerHTML += pirkums;
+    };
+
+};
